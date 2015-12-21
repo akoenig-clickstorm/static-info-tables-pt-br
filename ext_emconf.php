91d3c7a3a4f8e814b3687610266fa7fd9e8d5044
Florian Wessels <f.wessels@bitmotion.de>
2015-12-21 14:14:05 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables_de".
+ *
+ * Auto generated 07-09-2015 16:40
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Static Info Tables (pt_BR)',
+	'description' => 'Brazilian portuguese (pt_BR) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'version' => '6.2.0',
+	'state' => 'stable',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Florian Wessels',
+	'author_email' => 'typo3-ext@bitmotion.de',
+	'author_company' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.0-6.2.99',
+			'static_info_tables' => '6.2.0-',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
---
+++ ext_tables.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE static_countries (
+  cn_short_pt_BR varchar(100) DEFAULT '' NOT NULL
+);
+
