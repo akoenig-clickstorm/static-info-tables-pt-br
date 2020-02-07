327130a9fe6e1979257c6e604fad381bf606c9a1
Florian Wessels <f.wessels@bitmotion.de>
2020-02-07 11:59:43 +0100

    [BUGFIX] Use proper namespace

--- Configuration/TCA/Overrides/static_countries.php
+++ Configuration/TCA/Overrides/static_countries.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') || die;
 
 call_user_func(
     function ($additionalFields, $dataSetName) {
-        \Bitmotion\StaticInfoTablesAr\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
+        \Bitmotion\StaticInfoTablesPtBr\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
     },
     ['cn_short_en' => 'cn_short_pt_br'],
     'static_countries'
