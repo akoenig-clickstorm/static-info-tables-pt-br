f4dd7478543dd0102f6795b657f41ba30f08c4f3
Max Rösch <github@elbebass.com>
2022-07-22 12:42:05 +0200

    [TASK] Adapt dependecies for latest TYPO3 major version

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,26 +4,24 @@ $EM_CONF['static_info_tables_pt_br'] = [
     'title' => 'Static Info Tables (pt_BR)',
     'description' => 'Brazilian portuguese (pt_BR) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '6.9.1',
+    'version' => '7.0.0',
     'state' => 'stable',
-    'uploadfolder' => false,
-    'createDirs' => '',
-    'clearcacheonload' => false,
+    'clearCacheOnLoad' => 1,
     'author' => 'Max Rösch',
     'author_email' => 'dev@Leuchtfeuer.com',
     'author_company' => 'Leuchtfeuer Digital Marketing',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'php' => '7.2.0-7.4.99',
-            'static_info_tables' => '6.9.0-6.99.99',
+            'static_info_tables' => '6.7.3-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
     ],
     'autoload' => [
         'psr-4' => [
-            'Bitmotion\\StaticInfoTablesPtBr\\' => 'Classes'
-        ]
+            'Bitmotion\\StaticInfoTablesPtBr\\' => 'Classes',
+        ],
     ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,12 @@
 <?php
-defined('TYPO3_MODE') || die;
+defined('TYPO3_MODE') || die();
 
-call_user_func(
-    function($extensionKey) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.typoscript">');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">');
-    }, \Bitmotion\StaticInfoTablesPtBr\Extension::EXTENSION_KEY
-);
-
+call_user_func(function ($extensionKey) {
+    // Deprecated since 10.0 @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.typoscript">'
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
+    );
+}, \Bitmotion\StaticInfoTablesPtBr\Extension::EXTENSION_KEY);
